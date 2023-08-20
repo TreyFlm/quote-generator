36e8fe424e131c1edc8455a93e948fd637a035ff
Trey Fleming <treyFleming02@gmail.com>
2023-08-20 13:06:24 -0500

    Added owners of quotes, a font for the text, and made the button hover

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let quotes = [
-  'Dedication makes dreams come true.',
-  'Once you know what failure feels like, determination chases success.',
-  'You have to fight to reach your dream. You have to sacrifice and work hard for it.',
-  'You have to be able to accept failure to get better.',
-  "Things don't correct themselves, you've got to go out there and work hard to correct them",
-  "I can accept failure, everyone fails at something. But I can't accept not trying.",
-  'Winning is not always the barometer of getting better',
-  'Persistence is very important; you should not give up unless you are forced to give up.',
-  "Don't compare yourself with anyone in this world ... if you do so, you are insulting yourself.",
-  "Life's too short to hang out with people who aren't resourceful.",
+  ' "Dedication makes dreams come true". - Kobe Bryant',
+  '"Once you know what failure feels like, determination chases success". - Kobe Bryant',
+  ' "You have to fight to reach your dream. You have to sacrifice and work hard for it". - Leo Messi',
+  ' "You have to be able to accept failure to get better". - LeBron James',
+  ' "Things don\'t correct themselves, you\'ve got to go out there and work hard to correct them" - Tom Brady',
+  ' "I can accept failure, everyone fails at something. But I can\'t accept not trying". - Michael Jordan',
+  ' "Winning is not always the barometer of getting better". - Tiger Woods',
+  ' "Persistence is very important; you should not give up unless you are forced to give up". - Elon Musk',
+  ' "Don\'t compare yourself with anyone in this world ... if you do so, you are insulting yourself". - Bill Gates',
+  ' "Life\'s too short to hang out with people who aren\'t resourceful". - Jeff Bezos',
 ];
 
 document.querySelector('#gen-btn').addEventListener('click', genQuote);
